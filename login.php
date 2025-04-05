81cfe3ca1d2481f7609e0cbd1df4a23643432261
Tr3vorihno <enricocervi6@gmail.com>
2025-04-05 13:10:14 +0200

    aggiunta db

--- login.php
+++ login.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>login</title>
 </head>
 <body>
-    <h1>sei dentro per ora</h1>
+    <?php
+        
+    ?>
 </body>
 </html>
